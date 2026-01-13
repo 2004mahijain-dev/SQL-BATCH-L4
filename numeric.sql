01ce9400031a6fa4a9f605495e62b1564cb7a6d0
2004mahijain-dev <2004mahijain@gmail.com>
2026-01-13 10:12:03 +0530

    Add files via upload

---
+++ numeric.sql
@@ -0,0 +1,49 @@
+-- last session constraints (foreign key)
+-- data type in sql
+-- numeric => int, float, decimal
+-- tinyint, medium, big int, small int, int
+-- 1 byte, small=> 2 byte, medium => 3, int => 4 byte, long 8 byte
+-- 1 byte => 8 bit (2**8) => 256
+
+use regex1;
+create table test10( id tinyint);
+insert into test10 values(1), (-128), (127);
+insert into test11 values(129), (255);
+
+create table test11( id tinyint unsigned);
+insert into test11 values(129), (255);
+select * from test11;
+
+create table test12( price float);
+insert into test12 values(9182.125), (10.120381232);
+select * from test12;
+
+create table test13(price float, price2 double );
+insert into test13 values(9182.125, 9182.125), (10.120381232, 10.1203812320);
+select * from test13;
+
+create table test14( price2 double(5,2));
+
+insert into test14 values(98.1);
+insert into test14 values(98.15);
+insert into test14 values(4.11567);
+insert into test14 values(4.11567);
+insert into test14 values(195.11567);
+insert into test14 values(1955.12312); -- error
+
+select * from test14;
+
+-- varchar and char
+-- varchar is datatype => strings / character values
+-- char => character but of fix length of character
+
+create table test156( name char(10));
+insert into test156 values('abc'),(defghi);
+insert into test156 values('aqwerfdsazx');  -- error
+select * from test156;
+
+insert into test156 values(
+insert into test156 values(''
+select * from test156;
+
+
